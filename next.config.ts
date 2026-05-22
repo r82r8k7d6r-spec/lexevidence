f9cf5ce1d0c903e8aa4b001fca6c97fb99ea5d02
r82r8k7d6r-spec <r82r8k7d6r@privaterelay.appleid.com>
2026-05-23 00:06:07 +0900

    fix: next.config.tsからapiプロパティを削除

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,6 @@ const nextConfig: NextConfig = {
   experimental: {
     serverActionsBodySizeLimit: '100mb',
   },
-  api: {
-    bodyParser: {
-      sizeLimit: '100mb',
-    },
-    responseLimit: '100mb',
-  },
 };
 
 export default nextConfig;
